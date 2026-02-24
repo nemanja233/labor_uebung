1234224a220c53ef4e05a66deb43ca9372880d57
nemanja233 <neca.nemanja.2007@gmail.com>
2026-02-24 08:39:10 +0100

    Branch

--- custom_module.py
+++ custom_module.py
@@ -1,7 +1,16 @@
-def say_hi(name):
-    for i in range(1000000000):
-        print(f"Hallo {name}, willkommen! Schön, dass du da bist.")
+import customtkinter as ctk
+app = ctk.CTk()
+app.title("say hi")
+app.geometry("175x75")
+
+def say_hi():
+    for i in range(10):
+        print(f"Hallo du, willkommen! Schön, dass du da bist.")
         i+=1
 
 
-say_hi("UR NAME HERE")
+button = ctk.CTkButton(app, text="push me", command=say_hi)
+button.grid(row=0, column=0, padx=20, pady=20)
+
+
+app.mainloop()
